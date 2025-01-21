e7cb6a69150ed2161b891ab0d512a43e5462eb43
PURE Cupcakes Desktop 2 <purecupcakes-desktop2@PUREs-iMac.local>
2025-01-21 12:16:07 -0800

    debugging

--- script.js
+++ script.js
@@ -14,7 +14,8 @@ function addTask() {
     }
 }
 
-document.getElementById('taskInput').addEventListener('keypress', function(event) {
+// Allows user to submit task by pressing Enter key
+document.getElementById('taskInput').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') { // Check if Enter key is pressed
         addTask();
     }
