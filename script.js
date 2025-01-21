19a581ef2dee4795d29aa70e7b4d08125ffeb697
PURE Cupcakes Desktop 2 <purecupcakes-desktop2@PUREs-iMac.local>
2025-01-21 12:18:14 -0800

    Added protections agains accidental form submission

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function addTask() {
 // Allows user to submit task by pressing Enter key
 document.getElementById('taskInput').addEventListener('keydown', function(event) {
     if (event.key === 'Enter') { // Check if Enter key is pressed
+        event.preventDefault(); // Prevents accidental form submission
         addTask();
     }
 });
